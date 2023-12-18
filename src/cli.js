92e48200d24ab217a8013ac3158f431f4c26baeb
Marucami <samotskiy2002@mail.ru>
2023-12-18 23:27:18 +0300

    finale?

--- src/cli.js
+++ src/cli.js
@@ -6,16 +6,3 @@ export function greetings() {
   console.log(`Hello, ${name}`);
   return name;
 }
-
-export const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
-
-export const brainGameStart = (PointsTiWin, PlayerName, gameName) => {
-  let correctCount = 0;
-  while (correctCount < PointsTiWin) {
-    if (gameName(PlayerName) === 1) {
-      correctCount += 1;
-    } else return 0;
-  }
-  console.log(`Congratulations, ${PlayerName}!`);
-  return 0;
-};
