4098d3db9debbd25e73bcdda4c8a912aded27f00
Marucami <samotskiy2002@mail.ru>
2023-12-18 21:34:12 +0300

    Tryouts

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,38 @@
 #!/usr/bin/env node
-import { PlayProgression } from '../games/Brain-progression-carkas.js';
+import readlineSync from 'readline-sync';
+import { greetings, brainGameStart } from '../src/cli.js';
 
-PlayProgression();
+function generateArithmeticProgression() {
+  const length = Math.floor(Math.random() * 6) + 5;
+  const start = Math.floor(Math.random() * 11);
+  const diff = Math.floor(Math.random() * 11);
+  const progression = [start];
+  for (let i = 1; i < length; i += 1) {
+    progression.push(start + diff * i);
+  }
+  return progression;
+}
+
+const PlayerName = greetings();
+
+function PlayProgression() {
+  const progression = generateArithmeticProgression();
+  const hiddenIndex = Math.floor(Math.random() * progression.length);
+  const hiddenValue = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+  console.log(`Question: ${progression.join(' ')}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const correctAnswer = hiddenValue;
+  if (parseInt(userAnswer, 10) === correctAnswer) {
+    console.log('Correct!');
+    return 1;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${PlayerName}!`);
+  return 0;
+}
+
+const PointsToWin = 3;
+const gameName = PlayProgression;
+console.log('What number is missing in the progression?');
+brainGameStart(PointsToWin, PlayerName, gameName);
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,18 @@
 import readlineSync from 'readline-sync';
 
-function greetings() {
+export function greetings() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 }
 
-export { greetings };
+export function brainGameStart(PointsToWin, PlayerName, gameName) {
+  let correctCount = 0;
+  while (correctCount < PointsToWin) {
+    if (gameName(PlayerName) === 1) correctCount += 1;
+    else return 0;
+  }
+  console.log(`Congratulations, ${PlayerName}!`);
+  return 0;
+}
